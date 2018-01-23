1dea9cca0003adbbe820a58030fce44ee5d83cbe
song xiuhe <songxiuhe@lenovo.com>
2018-01-23 17:58:35 +0800

    change data_analyse.c for str_*_call

--- data_analyse.c
+++ data_analyse.c
@@ -1,30 +1,44 @@
-//rececive data from 
-//sip:101@10.100.9.233: Incoming call from: 103 sip:103@192.168.150.108 - (press 'a' to accept)
-//sip:103@192.168.150.108: session closed: Connection reset by peer
-//sip:101@10.100.9.233: Incoming call from: 103 sip:103@192.168.150.108 - (press 'a' to accept)
-//b
-//call: rejecting incoming call from sip:103@192.168.150.108 (486 Rejected)
 
 #include <string.h>
 
 #include <stdio.h>
 
+const char str_accept_call[] = "ola_incoming_call";     //来电话
+const char str_reject_call[] = "ola_rejecting_call";    //拒接电话
+const char str_cancle_call[] = "ola_cancle_call";       //对方取消电话
+const char str_connec_call[] = "ola_connecting_call";   //打电话时电话正在接通
+const char str_hangup_call[] = "ola_hangup_call";       //挂断电话
+const char str_estblh_call[] = "ola_established_call";  //电话已接通
 int data_analyse(const char * data)
 {
 
-    const char str_accept[] = "- (press 'a' to accept)";
-   // const char str_incoming[] = "- (press 'a' to accept)";
-   const char str_reset_by_peer[] = "session closed: Connection reset by peer";
-   const char str_rejecting_incoming_call[] = "rejecting incoming call ";
-
     char * p = NULL;
-    p = strstr(data , str_accept);
+    p = strstr(data , str_accept_call);//not find return null;else return point
     if(p)
         printf("**********来电话了*********  \n");
-    p = strstr(data , str_reset_by_peer);
+
+
+    p = strstr(data , str_connec_call);
     if(p)
-        printf("**********对方挂电话了*********  \n");
-    p = strstr(data , str_rejecting_incoming_call);
+        printf("**********电话正在连接*********  \n");
+   
+   
+    p = strstr(data , str_hangup_call);
+    if(p)
+        printf("**********挂断电话*********  \n");
+   
+   
+    p = strstr(data , str_estblh_call);
+    if(p)
+        printf("**********电话接通了*********  \n");
+   
+   
+    p = strstr(data , str_cancle_call);
+    if(p)
+        printf("**********对方取消通话*********  \n");
+   
+   
+    p = strstr(data , str_reject_call);
     if(p)
         printf("**********您拒接了*********  \n");
 
